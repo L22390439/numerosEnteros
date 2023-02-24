bea6baee5710a0eec78316a5366c42907c241058
L22390439 <L22390439@chetumal.tecnm.mx>
2023-02-24 11:20:21 -0500

    Initial commit

---
+++ numerosEneteros.java
@@ -0,0 +1,28 @@
+public class numerosEnteros {
+    public static void main(String[] args) {
+        //programa que pida dos numeros enteros y determine si la diferencia entre ellos es un divisor exacto de ambos.
+        sistema. fuera. println ("numeros enteros");
+    Numeros enteros = new numerosEnteros(caso 1, caso 2);
+
+    System.out.println("Los numeros num1, num2 del caso 1 es: ")
+            ++ programa.calcula
+    programa = null;
+
+    }
+}
+class Caso1{
+    int num1;
+    int num2;
+
+    protected Caso1 (int num1, int num2){
+        this.num1=40;
+        this.num2=30;
+    }
+public int calcula(){
+        int diferencia = (this.num1-this.num2);
+
+        int cociente num1 = (num1/diferencia);
+        int cociente num2 = (num2/diferencia);
+  return cociente;
+}
+}
